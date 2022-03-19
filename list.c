7f21f76281c22f5499d3281abbd00091ab404825
SebaSuffer <>
2022-03-19 23:56:39 +0000

    new try

--- list.c
+++ list.c
@@ -56,8 +56,8 @@ void * lastList(List * list) {
 
 void * prevList(List * list) {
   if (list->current != NULL && list->current->prev != NULL)
-    return NULL;
-  return list->current->prev->data;
+    return list->current->prev->data;
+  return NULL;
 }
 
 void pushFront(List * list, void * data) {
