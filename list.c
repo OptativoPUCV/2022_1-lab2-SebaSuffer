3f8b19270cc13a0d6c5d8a23027462135bd433d6
SebaSuffer <>
2022-03-20 16:57:12 +0000

    new try

--- list.c
+++ list.c
@@ -46,7 +46,7 @@ void * nextList(List * list) {
   if (list->current != NULL && list->current->next != NULL)
   {
     list->current = list->current->next;
-    return list->current->next->data;
+    return list->current->data;
   }
   return NULL;
 }
@@ -64,7 +64,7 @@ void * prevList(List * list) {
   if (list->current != NULL && list->current->prev != NULL)
   {
     list->current = list->current->prev;
-    return list->current->prev->data;
+    return list->current->data;
   }
   return NULL;
 }
