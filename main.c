9cc922625bc3c61d4653a589f74e8a8c8201aee5
Sebastian <101427281+SebaSuffer@users.noreply.github.com>
2022-05-02 19:35:29 -0400

    Update main.c

--- main.c
+++ main.c
@@ -9,6 +9,53 @@ typedef struct{
     char director[30];
 }Pelicula;
 
+/*
+Mostrar todos los productos (): La aplicación muestra por pantalla 
+todos los productos existentes (y su respectiva información).
+*/
+void mostrarTodoslosProductos(Stock * almacen){
+    system("cls");
+    Pair * aux = firstMap(almacen->nombre);
+    printf("=============Productos Disponibles=============\n\n");
+    while (aux != NULL){
+        printf("Producto: %s\n", (((Producto *)(Pair *)aux->value)->nombre));
+        printf(" -Stock Disponible: %zd\n", (((Producto *)(Pair *)aux->value)->stock));
+        printf(" -Valor: %zd\n", (((Producto *)(Pair *)aux->value)->valor));
+        printf(" -Tipo: %s\n", (((Producto *)(Pair *)aux->value)->tipo));
+        printf(" -Marca: %s\n", (((Producto *)(Pair *)aux->value)->marca));
+        printf("\n");
+
+        aux = nextMap(almacen->nombre);
+    }
+    printf("\n");
+    printf("================FIN DE LA LECTURA================");
+
+    getch();
+}
+
+/*
+Mostrar carritos de compra: Se muestran los nombres de los carritos 
+de compra creados y la cantidad de productos que tiene cada uno de ellos. 
+*/
+void mostrarCarritosDeCompra(Carritos* carritosDeCompra){
+    system("cls");
+    size_t i=1;
+    Pair * aux = firstMap(carritosDeCompra->arrCarritos);
+    printf("=============[Carritos de Compra]=============\n\n");
+    while (aux != NULL){
+        printf("%u.- %s\n", i, ((Carrito*)aux->value)->nombre);
+        printf(" -Cantidad Disponible = %zd\n", ((Carrito*)aux->value)->cantProductos);
+        printf("\n");
+        aux = nextMap(carritosDeCompra->arrCarritos);
+        i++;
+    }
+
+    printf("==============[FIN DE LA LECTURA]==============");
+
+    getch();
+}
+
+
 
 int main( int argc, char *argv[] ){
 
